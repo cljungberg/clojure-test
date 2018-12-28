e27a4b7b2c778a7b03bfdb28463736f5ed1a501a
Christina Ljungberg <christina.ljungberg@dotnetmentor.se>
2018-12-28 11:32:26 +0100

    Can run tests from REPL

--- clojure-test/test/test/user_model_test.clj
+++ clojure-test/test/test/user_model_test.clj
@@ -3,14 +3,18 @@
             [test.core :refer :all]
             [test.user-model :as um]))
 
-(defmacro is-not-thrown? [e expr]
-  `(is (not ('thrown? ~e ~expr))))
-
 (deftest user-model-gives-correct-name-back
-  (testing "Validate that Christina is valid"
+  (testing "Validate that Christina's name and are are valid"
     (is (= "Christina" (:name (um/get-user-model "Christina" 47 2 "clojure"))))
     (is (= 47 (:age (um/get-user-model "Christina" 47 2 "clojure"))))))
 
 (deftest user-model-not-ok-negative-age
   (testing "Validate that errors are thrown when age is negative"
     (is (thrown? Exception (um/get-user-model "Christina" -47 2 "clojure")))))
+
+
+
+; Run tests:
+; (require 'test.user-model-test :reload-all)
+; (in-ns 'test.user-model-test)
+; (run-tests)
